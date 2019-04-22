38f99c06cfab48eb94c8e4102830f80d580a6867
chelseajia <jiahuibo@seattleu.edu>
2019-04-21 23:22:38 -0700

    edited data

--- empdata.js
+++ empdata.js
@@ -3,113 +3,113 @@
     {
       "name": "Tony Stark",
       "gender": "Male",
-      "current alias": "Iron Man",
-      "marital status": "Single",
+      "current_alias": "Iron Man",
+      "marital_status": "Single",
       "citizenship": "American"
     },
     {
       "name": "Carol Danvers",
       "gender": "Female",
-      "current alias": "Captain Marvel",
-      "marital status": "Single",
+      "current_alias": "Captain Marvel",
+      "marital_status": "Single",
       "citizenship": "American"
     },
     {
       "name": "Steven Rogers",
       "gender": "Male",
-      "current alias": "Captain America",
-      "marital status": "Single",
+      "current_alias": "Captain America",
+      "marital_status": "Single",
       "citizenship": "American"
     },
     {
       "name": "James Howlett",
       "gender": "Male",
-      "current alias": "Wolverine",
-      "marital status": "Widowed",
+      "current_alias": "Wolverine",
+      "marital_status": "Widowed",
       "citizenship": "Canadian"
     },
     {
       "name": "T'Challa",
       "gender": "Male",
-      "current alias": "Black Panther",
-      "marital status": "Single",
+      "current_alias": "Black Panther",
+      "marital_status": "Single",
       "citizenship": "Wakandan"
     },
     {
       "name": "Natalia Romanova",
       "gender": "Female",
-      "current alias": "Black Widow",
-      "marital status": "Divorced",
+      "current_alias": "Black Widow",
+      "marital_status": "Divorced",
       "citizenship": "Russian, American"
     },
     {
      "name": "Clinton Barton",
       "gender": "Male",
-      "current alias": "Hawkeye",
-      "marital status": "Divorced",
+      "current_alias": "Hawkeye",
+      "marital_status": "Divorced",
       "citizenship": "American"
     },
     {
       "name": "Wanda Maximoff",
       "gender": "Female",
-      "current alias": "Scarlet Witch",
-      "marital status": "Divorced",
+      "current_alias": "Scarlet Witch",
+      "marital_status": "Divorced",
       "citizenship": "American"
     },
     {
       "name": "Pietro Maximoff",
       "gender": "Male",
-      "current alias": "Quicksilver",
-      "marital status": "Single",
+      "current_alias": "Quicksilver",
+      "marital_status": "Single",
       "citizenship": "American"
     },
     {
       "name": "Thor Odinson",
       "gender": "Male",
-      "current alias": "God of Thunder",
-      "marital status": "Single",
+      "current_alias": "God of Thunder",
+      "marital_status": "Single",
       "citizenship": "Asgardian"
     },
     {
       "name": "Wade Wilson",
       "gender": "Male",
-      "current alias": "Deadpool",
-      "marital status": "Married",
+      "current_alias": "Deadpool",
+      "marital_status": "Married",
       "citizenship": "Canadian"
     },
     {
       "name": "Janet van Dyne",
       "gender": "Female",
-      "current alias": "The Wasp",
-      "marital status": "Divorced",
+      "current_alias": "The Wasp",
+      "marital_status": "Divorced",
       "citizenship": "American"
     },
     {
       "name": "Henry Pym",
       "gender": "Male",
-      "current alias": "Ant-Man",
-      "marital status": "Divorced",
+      "current_alias": "Ant-Man",
+      "marital_status": "Divorced",
       "citizenship": "American"
     },
     {
       "name": "Scott Summers",
       "gender": "Male",
-      "current alias": "Cyclops",
-      "marital status": "Married",
+      "current_alias": "Cyclops",
+      "marital_status": "Married",
       "citizenship": "American"
     },
     {
       "name": "Max Eisenhardt",
       "gender": "Male",
-      "current alias": "Magneto",
-      "marital status": "Widowed",
+      "current_alias": "Magneto",
+      "marital_status": "Widowed",
       "citizenship": "German"
     },
     {
       "name": "Charles Xavier",
       "gender": "Male",
-      "current alias": "X",
-      "marital status": "Divorced",
+      "current_alias": "X",
+      "marital_status": "Divorced",
       "citizenship": "American"
     }
   ]
